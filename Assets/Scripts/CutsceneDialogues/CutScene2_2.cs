c0fe560146a4d501f31b897bb2bbae6736f1d4d8
Joshuaflores12 <joshuagabriellflores@gmail.com>
2026-02-11 04:00:57 +0800

    feat(core): AutomatedPhoneScript-Joshua
    Automated Phone script to the following
    Chapter 1 - CutScene1, Exploration 1.1, CutScene 6
    Chapter 2 Cutscene 2.2
    Base Phone UI implemented on all Chapters, open phone via Button
    Taho Minigame, Research Panic (Debugged)

--- Assets/Scripts/CutsceneDialogues/CutScene2_2.cs
+++ Assets/Scripts/CutsceneDialogues/CutScene2_2.cs
@@ -8,6 +8,13 @@ public class CutScene2_2 : MonoBehaviour
     public GameObject _DialoguePanel;
     public TextMeshProUGUI _NpcName;
     public TextMeshProUGUI _StoryText;
+    public GameObject _NotifPanel1;
+    public GameObject _NotifPanel2;
+    public GameObject _NotifPanel3;
+    public TextMeshProUGUI _NotifText1;
+    public TextMeshProUGUI _NotifText2;
+    public TextMeshProUGUI _NotifText3;
+    public GameObject _PhonePanel;
     [TextArea] public string _Storyline;
 
     [SerializeField] private int _DialogueIndex;
@@ -30,32 +37,26 @@ public class CutScene2_2 : MonoBehaviour
 
             if (_DialogueIndex == 1) 
             {
-                StartCoroutine(ShowNarratorNewDialogue("CAPSTONE PROGRESS REVIEW"));
-            }
-
-            else if (_DialogueIndex == 2)
-            {
-                StartCoroutine(ShowNarratorNewDialogue("Midterm submission required."));
+                _DialoguePanel.SetActive(false);
+                StartCoroutine(PhoneNotifRoutine());
             }
 
             else if (_DialogueIndex == 3)
             {
-                StartCoroutine(ShowNarratorNewDialogue("All groups must present updated scope and documentation."));
+                StartCoroutine(ShowNarratorNewDialogue("You scroll."));
+                _PhonePanel.SetActive(false);
             }
 
             else if (_DialogueIndex == 4)
             {
-                StartCoroutine(ShowNarratorNewDialogue("You scroll."));
+                StartCoroutine(ShowNarratorNewDialogue("Raya’s name is still there."));
+                _PhonePanel.SetActive(false);
             }
 
             else if (_DialogueIndex == 5)
             {
-                StartCoroutine(ShowNarratorNewDialogue("Raya’s name is still there."));
-            }
-
-            else if (_DialogueIndex == 6)
-            {
                 StartCoroutine(ShowNarratorNewDialogue("No explanation. Just a deadline."));
+                _PhonePanel.SetActive(false);
             }
 
             else 
@@ -102,5 +103,26 @@ public class CutScene2_2 : MonoBehaviour
     {
         yield return new WaitForSeconds(1f);
         SceneManager.LoadScene("Exploration 2.2");
+    }
+
+    IEnumerator PhoneNotifRoutine()
+    {
+        _PhonePanel.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        _NotifPanel1.SetActive(true);
+        _NotifText1.text = "CAPSTONE PROGRESS REVIEW";
+
+        yield return new WaitForSeconds(1f);
+        _NotifPanel1.SetActive(false);
+        _NotifPanel2.SetActive(true);
+        _NotifText2.text = "Midterm submission required.";
+
+        yield return new WaitForSeconds(1f);
+        _NotifPanel1.SetActive(false);
+        _NotifPanel2.SetActive(false);
+        _NotifPanel3.SetActive(true);
+        _NotifText3.text = "All groups must present updated scope and documentation.";
+        _DialogueIndex = 2;
+        _CanContinue = true;
     }
 }
--- Assets/Scripts/PhoneUIScripts/FadeOutMessages.cs
+++ Assets/Scripts/PhoneUIScripts/FadeOutMessages.cs
@@ -4,20 +4,20 @@ using TMPro;
 using TMPro.EditorUtilities;
 public class FadeOutMessages : MonoBehaviour
 {
-    public CanvasGroup _RayaMessageInputFieldUI1;
-    public CanvasGroup _RayaMessageInputFieldUI2;
-    public CanvasGroup _RayaMessageInputFieldUI3;
-    public TMP_InputField _RayaMessageInput1;
-    public TMP_InputField _RayaMessageInput2;
-    public TMP_InputField _RayaMessageInput3;
+    public GameObject _RayasMessageBox1;
+    public GameObject _RayasMessageBox2;
+    public GameObject _RayasMessageBox3;
+    public TextMeshProUGUI _RayasMessage1;
+    public TextMeshProUGUI _RayasMessage2;
+    public TextMeshProUGUI _RayasMessage3;
     public GameObject _Phone;
     public CutScene1 _DialogueIndex;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        _RayaMessageInputFieldUI1.alpha = 0f;
-        _RayaMessageInputFieldUI2.alpha = 0f;
-        _RayaMessageInputFieldUI3.alpha = 0f;
+        _RayasMessageBox1.SetActive(false);
+        _RayasMessageBox2.SetActive(false);
+        _RayasMessageBox3.SetActive(false);
     }
 
     private void Update()
@@ -30,21 +30,21 @@ public class FadeOutMessages : MonoBehaviour
     {
         yield return new WaitForSeconds(1f);
 
-        _RayaMessageInputFieldUI1.alpha = 1f;
-        _RayaMessageInput1.text = "Don’t forget the paper, sleepyhead.";
+        _RayasMessageBox1.SetActive(true);
+        _RayasMessage1.text = "Don’t forget the paper, sleepyhead.";
 
         yield return new WaitForSeconds(1f);
 
-        _RayaMessageInputFieldUI2.alpha = 1f;
-        _RayaMessageInput2.text = "Also… can we talk later";
+        _RayasMessageBox2.SetActive(true);
+        _RayasMessage2.text = "Also… can we talk later";
 
         yield return new WaitForSeconds(1f);
 
-        _RayaMessageInputFieldUI3 .alpha = 1f;
-        _RayaMessageInput3.text = "hey, are you there?";
+        _RayasMessageBox3.SetActive(true);
+        _RayasMessage3.text = "hey, are you there?";
 
         yield return new WaitForSeconds(1.5f);
-        _DialogueIndex._DialogueIndex = 4;
+        _DialogueIndex._DialogueIndex = 2;
         _DialogueIndex._CanContinue = true;
     }
 
